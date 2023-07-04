c1eb4fa4aa40055e1318555f43bd8d94b9be01ec
Andrea Pucci <andreacpucci@gmail.com>
2023-07-04 12:46:03 -0300

    nueva organización

---
+++ src/Views/Contact/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <footer class="foot">
+            <h5>2023 © All rights reserved.</h5>
+            <div class="logo">
+                <a href=""><img class="imglogo" src="./imagenes/footer-logo.png" alt="logo"/></a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
